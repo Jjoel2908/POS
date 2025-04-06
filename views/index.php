74d26e35352bed55ffd01f39740b0bad3d151b57
Joel <joelalvaradom02@gmail.com>
2025-04-06 12:16:48 -0600

    LOGIN

--- views/index.php
+++ views/index.php
@@ -3,13 +3,12 @@
 <!-- [  # L O G I N #  ] -->
 <div class="row justify-content-center align-items-center vh-100 mx-4">
 
-   <div class="container-login text-center p-4">
-      <img class="img-fluid" src="../public/images/logo.jpg" alt="Logo">
-      <h2 class="my-3">POS</h2>
-
+   <div class="container-login text-center p-4 mb-2">
+      <h2 class="fw-bold text-secondary my-4">Bienvenido</h2>
+      <h3 class="title-login">Gastelum Jr</h3>
       <form method="post" id="formLogin" action="">
-         <input type="text" id="user" name="user" class="form-control my-4" placeholder="Usuario">
-         <input type="password" id="password" name="password" class="form-control my-4" placeholder="Contraseña">
+         <input type="text" id="user" name="user" class="form-control my-4 container-input" placeholder="Usuario">
+         <input type="password" id="password" name="password" class="form-control my-4 container-input" placeholder="Contraseña">
          <button type="submit" class="btn-login my-3">Ingresar</button>
       </form>
    </div>
