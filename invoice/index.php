ef09a74b1a622385e9cc2646e2a7f137079ff3c6
Joel <joelalvaradom02@gmail.com>
2025-08-03 15:14:20 -0600

    DEBUG

--- invoice/index.php
+++ invoice/index.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 require_once '../config/global.php';
 require_once '../models/Invoice.php';
 require '../models/MYPDF.php';
